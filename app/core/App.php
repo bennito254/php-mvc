d276b6c87915427a355b7e9da5b2a39f3e5144c0
Ben Muriithi <benmuriithi929@gmail.com>
2019-10-20 14:02:17 +0300

    Update App.php
    Path-style access without GET but not ignoring it. http://localhost:3001/controller/method/param1/param2?q=1

--- app/core/App.php
+++ app/core/App.php
@@ -2,48 +2,61 @@
 
 class App
 {
-   protected $controller = 'home';
-   protected $method = 'index';
-   protected $params = [];
+   private $controller = 'home';
+   private $method = 'index';
+   private $params = [];
 
    public function __construct()
    {
       $url = $this->parseUrl();
-
-      if (isset($url)) {
-
-         if (file_exists(__DIR__ . '/../controllers/' . $url[0] . '.php')) {
-
-            $this->controller = $url[0];
-            unset($url[0]);
-
-            require_once __DIR__ . '/../controllers/' . $this->controller . '.php';
-
-            $this->controller = new $this->controller;
-
-            if (isset($url[1])) {
-               if (method_exists($this->controller, $url[1])) {
-                  $this->method = $url[1];
-                  unset($url[1]);
-               } else {
-                  exit('The controller does not exist.');
-               }
-            }
-
-            $this->params = array_values($url);
-
-            call_user_func_array([$this->controller, $this->method], $this->params);
-         } else {
-            exit('The streamed directory does not exist.');
-         }
+      
+      //Set controller if it exists
+      if (isset($url[0]) && $url[0] != '' ) {
+          $this->controller = $url[0];
+          unset($url[0]);
       }
+      
+      if (isset($url[1]) && $url[1] != '' ) {
+          $this->method = $url[1];
+          unset($url[1]);
+      }
+      
+     //Upper case the first character in controller
+     $this->controller = ucwords($this->controller);
+     
+     if (file_exists(__DIR__ . '/../controllers/' . $this->controller . '.php')) {
+        
+        require_once __DIR__ . '/../controllers/' . $this->controller . '.php';
+
+        $this->controller = new $this->controller;
+        
+        $this->params = array_values($url);
+
+        call_user_func_array([$this->controller, $this->method], $this->params);
+     } else {
+        exit('The controller {"'.$this->controller.'"} does not exist.');
+     }
+      
    }
 
    public function parseUrl()
    {
-      if (isset($_GET['url'])) {
-         $url = explode('/', filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL));
-         return $url;
-      }
+      //~ if (isset($_GET['url'])) {
+         //~ $url = explode('/', filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL));
+         //~ return $url;
+      //~ }
+      
+      $url = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '/';
+      
+      //remove GET params from the URL
+      if (($strpos = strpos($url, '?')) !== false) {
+			$url = substr($url, 0, $strpos);
+		}
+      
+      //Remove first slash
+      $url = substr($url, 1);
+      $url = explode('/', $url);
+      
+      return $url;
    }
 }
